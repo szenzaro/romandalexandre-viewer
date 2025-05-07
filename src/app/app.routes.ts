6fc81bfea3d8e8d0ca8c074e614a9da349930da6
Simone Zenzaro <s.zenzaro@gmail.com>
2025-05-07 12:32:28 +0200

    style: apply formatting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { ViewerComponent } from "./pages/viewer/viewer.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "**", component: ViewerComponent },
+];
