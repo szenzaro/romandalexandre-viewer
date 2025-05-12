2702e259cc6dfd176ed3f13ae05ab340f9cf84c6
Simone Zenzaro <s.zenzaro@gmail.com>
2025-05-12 17:25:12 +0200

    build: use primeng instead of bootstrap

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,25 @@ import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter, withComponentInputBinding } from "@angular/router";
 
 import { routes } from "./app.routes";
+import { provideHttpClient } from "@angular/common/http";
+import Aura from "@primeng/themes/aura";
+
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { providePrimeNG } from "primeng/config";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false || "none",
+        },
+      },
+    }),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(),
   ],
 };
