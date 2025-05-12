34e43f8858bed226c28d8695c9b359c0c4a92797
Simone Zenzaro <s.zenzaro@gmail.com>
2025-05-12 17:37:14 +0200

    feat: use filter service

--- src/app/pages/viewer/viewer.component.ts
+++ src/app/pages/viewer/viewer.component.ts
@@ -1,14 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { PageComponent } from "../../components/page/page.component";
 import { mockData } from "../../mockdata";
 import { WordsFilterComponent } from "../../components/words-filter/words-filter.component";
+import { OsdComponent } from "../../components/osd/osd.component";
+import { FilterService } from "../../services/filter.service";
 
 @Component({
   selector: "app-viewer",
-  imports: [PageComponent, WordsFilterComponent],
+  imports: [PageComponent, WordsFilterComponent, OsdComponent],
   templateUrl: "./viewer.component.html",
   styleUrl: "./viewer.component.scss",
 })
 export class ViewerComponent {
   pages = mockData; // TODO: change me
+  filterService = inject(FilterService);
 }
