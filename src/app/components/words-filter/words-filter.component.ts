7e872841f8258561fab2e8170bbe04a6e73d7946
Simone Zenzaro <s.zenzaro@gmail.com>
2025-05-14 14:56:21 +0200

    refactor: update style

--- src/app/components/words-filter/words-filter.component.ts
+++ src/app/components/words-filter/words-filter.component.ts
@@ -14,6 +14,7 @@ import {
   FilterSelection,
   WordsFilter,
 } from "../../services/filter.service";
+import { capitalize } from "../../models";
 
 @Component({
   selector: "app-words-filter",
@@ -122,7 +123,4 @@ export class WordsFilterComponent {
   }
 }
 
-function capitalize(str: string): string {
-  if (!str) return "";
-  return str[0].toUpperCase() + str.slice(1);
-}
+
